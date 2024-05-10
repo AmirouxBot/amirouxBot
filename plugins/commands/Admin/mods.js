89fe65bd161bd877d2bb1e30e59db4de1a65e2aa
AmirouxBot <132029601+AmirouxBot@users.noreply.github.com>
2024-05-11 00:19:29 +0100

    Update mods.js

--- plugins/commands/Admin/mods.js
+++ plugins/commands/Admin/mods.js
@@ -1,50 +1,137 @@
 const config = {
-    name: "الصيانه",
-    description: "on/off maintain mode",
-    usage: "[on/off]",
-    cooldown: 3,
+    name: "المسؤولين",
+    aliases: ["المسؤولين_عن_البوت"],
+    version: "1.0.1",
+    description: "List, Add or remove moderators",
     permissions: [2],
-    credits: "XaviaTeam",
-    isAbsolute: true
+    cooldown: 5
 }
 
 const langData = {
-    "vi_VN": {
-        "alreadyOn": "Bot đã ở trong chế độ bảo trì",
-        "on": "Đã bật chế độ bảo trì",
-        "alreadyOff": "Bot đã ở ngoài chế độ bảo trì",
-        "off": "Đã tắt chế độ bảo trì"
-    },
     "en_US": {
-        "alreadyOn": "Bot is already in maintain mode",
-        "on": "Turned on maintain mode",
-        "alreadyOff": "Bot is already out of maintain mode",
-        "off": "Turned off maintain mode"
+        "notAbsolute": "You are not an absolute moderator.",
+        "alreadyModerator": "This user is already a moderator.",
+        "notModerator": "This user is not a moderator.",
+        "missingTarget": "Please mention or reply someone.",
+        "add.success": "Added to moderator list:\n{added}",
+        "remove.success": "Removed from moderator list:\n{removed}",
+        "list": "Moderators:\n{moderators}",
+        "error": "Error: {error}"
+    },
+    "vi_VN": {
+        "notAbsolute": "Bạn không phải là quản trị viên tuyệt đối.",
+        "alreadyModerator": "Người dùng này đã là quản trị viên.",
+        "notModerator": "Người dùng này không phải là quản trị viên.",
+        "missingTarget": "Vui lòng nhắc đến hoặc trả lời một người.",
+        "add.success": "Đã thêm vào danh sách quản trị viên:\n{added}",
+        "remove.success": "Đã xóa khỏi danh sách quản trị viên:\n{removed}",
+        "list": "Quản trị viên:\n{moderators}",
+        "error": "Lỗi: {error}"
     },
     "ar_SY": {
-        "alreadyOn": "الروبوت في وضع الصيانة بالفعل",
-        "on": "تم تشغيل وضع الصيانة",
-        "alreadyOff": "البوت خارج وضع الصيانة بالفعل",
-        "off": "تم إيقاف تشغيل وضع الصيانة"
+        "notAbsolute": "أنت لست المسؤول المطلق.",
+        "alreadyModerator": "هذا المستخدم هو بالفعل مسؤول.",
+        "notModerator": "هذا المستخدم ليس مسؤولاً.",
+        "missingTarget": "يرجى ذكر أو الرد على شخص.",
+        "add.success": "تمت الإضافة إلى قائمة المسؤولين:\n{added}",
+        "remove.success": "تمت إزالته من قائمة السمؤولين:\n{removed}",
+        "list": "المسؤولين:\n{moderators}",
+        "error": "خطأ: {error}"
     }
 }
 
 async function onCall({ message, args, getLang }) {
-    let input = args[0]?.toLowerCase() == "on" ? true : args[0]?.toLowerCase() == "off" ? false : null;
+    const { type, messageReply, mentions, senderID, reply } = message;
 
-    if (input == null) input = !global.maintain;
+    try {
+        const isAbsolute = global.config.ABSOLUTES.some(id => id == senderID);
 
-    if (input) {
-        if (global.maintain) return message.reply(getLang("alreadyOn"));
-        global.maintain = true;
+        let query = args[0]?.toLowerCase();
+        switch (query) {
+            case "add":
+                {
+                    if (!isAbsolute) return reply(getLang("notAbsolute"));
 
-        message.reply(getLang("on"));
-    } else {
-        if (!global.maintain) return message.reply(getLang("alreadyOff"));
-        global.maintain = false;
+                    let success = [];
+                    if (type == "message_reply") {
+                        let userID = messageReply.senderID;
+                        if (global.config.MODERATORS.some(id => id == userID)) return reply(getLang("alreadyModerator"));
+                        global.config.MODERATORS.push(String(userID));
+                        success.push({
+                            id: userID,
+                            name: (await global.controllers.Users.getInfo(userID))?.name || userID
+                        });
+                    } else if (Object.keys(mentions).length > 0) {
+                        for (const userID in mentions) {
+                            if (global.config.MODERATORS.some(id => id == userID)) continue;
+                            global.config.MODERATORS.push(String(userID));
+                            success.push({
+                                id: userID,
+                                name: mentions[userID].replace(/@/g, '')
+                            });
+                        }
+                    } else return reply(getLang("missingTarget"));
 
-        message.reply(getLang("off"));
+                    global.config.save();
+                    reply({
+                        body: getLang("add.success", { added: success.map(user => user.name).join(", ") }),
+                        mentions: success.map(user => ({ tag: user.name, id: user.id }))
+                    });;
+
+                    break;
+                }
+            case "remove":
+            case "rm":
+            case "delete":
+            case "del":
+                {
+                    if (!isAbsolute) return reply(getLang("notAbsolute"));
+
+                    let success = [];
+                    if (type == "message_reply") {
+                        let userID = messageReply.senderID;
+                        if (!global.config.MODERATORS.some(id => id == userID)) return reply(getLang("notModerator"));
+                        global.config.MODERATORS = global.config.MODERATORS.filter(id => id != userID);
+                        success.push({
+                            id: userID,
+                            name: (await global.controllers.Users.getInfo(userID))?.name || userID
+                        });
+                    } else if (Object.keys(mentions).length > 0) {
+                        for (const userID in mentions) {
+                            if (!global.config.MODERATORS.some(id => id == userID)) continue;
+                            global.config.MODERATORS = global.config.MODERATORS.filter(id => id != userID);
+                            success.push({
+                                id: userID,
+                                name: mentions[userID].replace(/@/g, '')
+                            });
+                        }
+                    } else return reply(getLang("missingTarget"));
+
+                    global.config.save();
+                    reply({
+                        body: getLang("remove.success", { removed: success.map(user => user.name).join(", ") }),
+                        mentions: success.map(user => ({ tag: user.name, id: user.id }))
+                    });;
+
+                    break;
+                }
+            default:
+                {
+                    let moderators = global.config.MODERATORS.map(async id => {
+                        let info = await global.controllers.Users.getInfo(id);
+                        return `${info?.name || id} (${id})`;
+                    });
+                    moderators = await Promise.all(moderators);
+
+                    reply(getLang("list", { moderators: moderators.join("\n") }));
+                    break;
+                }
+        }
+    } catch (error) {
+        reply(getLang("error", { error }));
     }
+
+    return;
 }
 
 export default {
